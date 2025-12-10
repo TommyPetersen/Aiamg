9910ab58a2b348a1600ac2dd1197a775687e500c
Tommy Petersen <tp@aia.dk>
2025-12-10 21:02:11 +0100

    Rettet saa grafik-skaermens konstruktoer nu kan kaldes med vindueslokation-x og -y.

--- Camera.java
+++ Camera.java
@@ -15,36 +15,54 @@ public class Camera{
     protected Screen screen;
 
     public Camera(double frontPlaneValue,
-		  double projectionPlaneValue,
-		  double projectionWindowWidth,
-		  double projectionWindowHeight,
-		  int w,
-		  int h) throws Exception{
+                  double projectionPlaneValue,
+                  double projectionWindowWidth,
+                  double projectionWindowHeight,
+                  int w,
+                  int h) throws Exception{
+        this(frontPlaneValue,
+             projectionPlaneValue,
+             projectionWindowWidth,
+             projectionWindowHeight,
+             w,
+             h,
+             w/2,
+             h/2);
+    }
 
-	if (frontPlaneValue <= 0.0){
-	    throw new Exception("The value for frontPlaneValue must be positive");
-	}
-	
-	double backPlaneValue = projectionPlaneValue + 2000.0;
-	
-	cameraBoundingVolume = new CameraBoundingVolume(frontPlaneValue,
-							backPlaneValue,
-							projectionPlaneValue,
-							projectionWindowWidth,
-							projectionWindowHeight);
+    public Camera(double frontPlaneValue,
+                  double projectionPlaneValue,
+                  double projectionWindowWidth,
+                  double projectionWindowHeight,
+                  int w,
+                  int h,
+                  int locationX,
+                  int locationY) throws Exception{
 
-	ProjectionPlaneValue = projectionPlaneValue;
-	ProjectionWindowWidth = projectionWindowWidth;
-	ProjectionWindowHeight = projectionWindowHeight;
-	
-	if (w <= 0 || h <= 0){
-	    throw new Exception("The values for W and H must be positive");
-	}
-	
-	zBuffer = new ZBuffer(w, h, Double.MAX_VALUE, Color.black, backPlaneValue);
-	screen = new Screen(w, h);
+        if (frontPlaneValue <= 0.0){
+            throw new Exception("The value for frontPlaneValue must be positive");
+        }
 
-	transformationReset();
+        double backPlaneValue = projectionPlaneValue + 2000.0;
+
+        cameraBoundingVolume = new CameraBoundingVolume(frontPlaneValue,
+                                                        backPlaneValue,
+                                                        projectionPlaneValue,
+                                                        projectionWindowWidth,
+                                                        projectionWindowHeight);
+
+        ProjectionPlaneValue = projectionPlaneValue;
+        ProjectionWindowWidth = projectionWindowWidth;
+        ProjectionWindowHeight = projectionWindowHeight;
+
+        if (w <= 0 || h <= 0){
+            throw new Exception("The values for W and H must be positive");
+        }
+
+        zBuffer = new ZBuffer(w, h, Double.MAX_VALUE, Color.black, backPlaneValue);
+        screen = new Screen(w, h, locationX, locationY);
+
+        transformationReset();
     }
 
     public void turnOff(){
@@ -52,150 +70,150 @@ public class Camera{
     }
     
     public ZBuffer getZBuffer(){
-	return zBuffer;
+        return zBuffer;
     }
 
     public Screen getScreen(){
-	return screen;
+        return screen;
     }
 
     public Insets getInsetsOnScreen(){
-	return screen.getInsets();
+        return screen.getInsets();
     }
 
     public Point2D getPointInProjectionPlaneFromPointOnScreen(int w, int h){
-	int zBuf_i = 0;
-	int zBuf_j = 0;
+        int zBuf_i = 0;
+        int zBuf_j = 0;
 
-	int W = screen.getW();
-	int H = screen.getH();
+        int W = screen.getW();
+        int H = screen.getH();
 
-	zBuf_i = w;
-	zBuf_j = H - h - 1;
+        zBuf_i = w;
+        zBuf_j = H - h - 1;
 
         int point2d_x = (int) Math.round(((zBuf_i * ProjectionWindowWidth) / (W - 1)) - (ProjectionWindowWidth / 2.0));
-	int point2d_y = (int) Math.round(((zBuf_j * ProjectionWindowHeight) / (H - 1)) - (ProjectionWindowHeight / 2.0));
-	return new Point2D(point2d_x, point2d_y);
+        int point2d_y = (int) Math.round(((zBuf_j * ProjectionWindowHeight) / (H - 1)) - (ProjectionWindowHeight / 2.0));
+        return new Point2D(point2d_x, point2d_y);
     }
 
     public synchronized void transformationReset(){
-	double[][] a44 = new double[4][4];
-	a44[0][0] = 1.0; a44[0][1] = 0.0; a44[0][2] = 0.0; a44[0][3] = 0.0;
-	a44[1][0] = 0.0; a44[1][1] = 1.0; a44[1][2] = 0.0; a44[1][3] = 0.0;
-	a44[2][0] = 0.0; a44[2][1] = 0.0; a44[2][2] = 1.0; a44[2][3] = 0.0;
-	a44[3][0] = 0.0; a44[3][1] = 0.0; a44[3][2] = 0.0; a44[3][3] = 1.0;
+        double[][] a44 = new double[4][4];
+        a44[0][0] = 1.0; a44[0][1] = 0.0; a44[0][2] = 0.0; a44[0][3] = 0.0;
+        a44[1][0] = 0.0; a44[1][1] = 1.0; a44[1][2] = 0.0; a44[1][3] = 0.0;
+        a44[2][0] = 0.0; a44[2][1] = 0.0; a44[2][2] = 1.0; a44[2][3] = 0.0;
+        a44[3][0] = 0.0; a44[3][1] = 0.0; a44[3][2] = 0.0; a44[3][3] = 1.0;
 
-	TransformationMatrix = new Matrix4x4(a44);
+        TransformationMatrix = new Matrix4x4(a44);
     }
     
     public synchronized void updateTransformationByTranslation(double x, double y, double z){
-	updateTransformationByTranslation(new Point3D(x, y, z));
+        updateTransformationByTranslation(new Point3D(x, y, z));
     }
     
     public synchronized void updateTransformationByTranslation(Point3D p){
-	double[][] a44 = new double[4][4];
+        double[][] a44 = new double[4][4];
 
-	a44[0][0] = 1.0; a44[0][1] = 0.0; a44[0][2] = 0.0; a44[0][3] = -p.x;
-	a44[1][0] = 0.0; a44[1][1] = 1.0; a44[1][2] = 0.0; a44[1][3] = -p.y;
-	a44[2][0] = 0.0; a44[2][1] = 0.0; a44[2][2] = 1.0; a44[2][3] = -p.z;
-	a44[3][0] = 0.0; a44[3][1] = 0.0; a44[3][2] = 0.0; a44[3][3] = 1.0;
+        a44[0][0] = 1.0; a44[0][1] = 0.0; a44[0][2] = 0.0; a44[0][3] = -p.x;
+        a44[1][0] = 0.0; a44[1][1] = 1.0; a44[1][2] = 0.0; a44[1][3] = -p.y;
+        a44[2][0] = 0.0; a44[2][1] = 0.0; a44[2][2] = 1.0; a44[2][3] = -p.z;
+        a44[3][0] = 0.0; a44[3][1] = 0.0; a44[3][2] = 0.0; a44[3][3] = 1.0;
 
-	Matrix4x4 A = new Matrix4x4(a44);
+        Matrix4x4 A = new Matrix4x4(a44);
 
-	TransformationMatrix = A.multiply4x4(TransformationMatrix);
+        TransformationMatrix = A.multiply4x4(TransformationMatrix);
     }
 
-    public synchronized void updateTransformationByXRotation(double theta){	
-	// The camera is rotated by degree theta hence points should be
-	// rotated by the opposite degree -theta:
-	double cosTheta = Math.cos(-theta);
-	double sinTheta = Math.sin(-theta);
+    public synchronized void updateTransformationByXRotation(double theta){
+        // The camera is rotated by degree theta hence points should be
+        // rotated by the opposite degree -theta:
+        double cosTheta = Math.cos(-theta);
+        double sinTheta = Math.sin(-theta);
 
-	double[][] a44 = new double[4][4];
+        double[][] a44 = new double[4][4];
 
-	a44[0][0] = 1.0; a44[0][1] = 0.0;       a44[0][2] = 0.0;      a44[0][3] = 0.0;
-	a44[1][0] = 0.0; a44[1][1] = cosTheta;  a44[1][2] = sinTheta; a44[1][3] = 0.0;
-	a44[2][0] = 0.0; a44[2][1] = -sinTheta; a44[2][2] = cosTheta; a44[2][3] = 0.0;
-	a44[3][0] = 0.0; a44[3][1] = 0.0;       a44[3][2] = 0.0;      a44[3][3] = 1.0;
+        a44[0][0] = 1.0; a44[0][1] = 0.0;       a44[0][2] = 0.0;      a44[0][3] = 0.0;
+        a44[1][0] = 0.0; a44[1][1] = cosTheta;  a44[1][2] = sinTheta; a44[1][3] = 0.0;
+        a44[2][0] = 0.0; a44[2][1] = -sinTheta; a44[2][2] = cosTheta; a44[2][3] = 0.0;
+        a44[3][0] = 0.0; a44[3][1] = 0.0;       a44[3][2] = 0.0;      a44[3][3] = 1.0;
 
-	Matrix4x4 A = new Matrix4x4(a44);
+        Matrix4x4 A = new Matrix4x4(a44);
 
-	TransformationMatrix = TransformationMatrix.multiply4x4(A);
+        TransformationMatrix = TransformationMatrix.multiply4x4(A);
     }
 
     public synchronized void updateTransformationByYRotation(double theta){
-	// The camera is rotated by degree theta hence points should be
-	// rotated by the opposite degree -theta:
-	double cosTheta = Math.cos(-theta);
-	double sinTheta = Math.sin(-theta);
+        // The camera is rotated by degree theta hence points should be
+        // rotated by the opposite degree -theta:
+        double cosTheta = Math.cos(-theta);
+        double sinTheta = Math.sin(-theta);
 
-	double[][] a44 = new double[4][4];
+        double[][] a44 = new double[4][4];
 
-	a44[0][0] = cosTheta; a44[0][1] = 0.0; a44[0][2] = -sinTheta; a44[0][3] = 0.0;
-	a44[1][0] = 0.0;      a44[1][1] = 1.0; a44[1][2] = 0.0;       a44[1][3] = 0.0;
-	a44[2][0] = sinTheta; a44[2][1] = 0.0; a44[2][2] = cosTheta;  a44[2][3] = 0.0;
-	a44[3][0] = 0.0;      a44[3][1] = 0.0; a44[3][2] = 0.0;       a44[3][3] = 1.0;
+        a44[0][0] = cosTheta; a44[0][1] = 0.0; a44[0][2] = -sinTheta; a44[0][3] = 0.0;
+        a44[1][0] = 0.0;      a44[1][1] = 1.0; a44[1][2] = 0.0;       a44[1][3] = 0.0;
+        a44[2][0] = sinTheta; a44[2][1] = 0.0; a44[2][2] = cosTheta;  a44[2][3] = 0.0;
+        a44[3][0] = 0.0;      a44[3][1] = 0.0; a44[3][2] = 0.0;       a44[3][3] = 1.0;
 
-	Matrix4x4 A = new Matrix4x4(a44);
+        Matrix4x4 A = new Matrix4x4(a44);
 
-	TransformationMatrix = TransformationMatrix.multiply4x4(A);
+        TransformationMatrix = TransformationMatrix.multiply4x4(A);
     }
 
     public synchronized void updateTransformationByZRotation(double theta){
-	// The camera is rotated by degree theta in the xy-plane.
-	// The following matrix rotates in the yx-plane
-	// (giving opposite rotation). Hence the sign for theta
-	// is already ok, because the transformation is about rotating
-	// points in the scene (which rotate opposite than the camera):
-	double cosTheta = Math.cos(theta);
-	double sinTheta = Math.sin(theta);
+        // The camera is rotated by degree theta in the xy-plane.
+        // The following matrix rotates in the yx-plane
+        // (giving opposite rotation). Hence the sign for theta
+        // is already ok, because the transformation is about rotating
+        // points in the scene (which rotate opposite than the camera):
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
 
-	double[][] a44 = new double[4][4];
+        double[][] a44 = new double[4][4];
 
-	a44[0][0] = cosTheta;  a44[0][1] = sinTheta; a44[0][2] = 0.0; a44[0][3] = 0.0;
-	a44[1][0] = -sinTheta; a44[1][1] = cosTheta; a44[1][2] = 0.0; a44[1][3] = 0.0;
-	a44[2][0] = 0.0;       a44[2][1] = 0.0;      a44[2][2] = 1.0; a44[2][3] = 0.0;
-	a44[3][0] = 0.0;       a44[3][1] = 0.0;      a44[3][2] = 0.0; a44[3][3] = 1.0;
+        a44[0][0] = cosTheta;  a44[0][1] = sinTheta; a44[0][2] = 0.0; a44[0][3] = 0.0;
+        a44[1][0] = -sinTheta; a44[1][1] = cosTheta; a44[1][2] = 0.0; a44[1][3] = 0.0;
+        a44[2][0] = 0.0;       a44[2][1] = 0.0;      a44[2][2] = 1.0; a44[2][3] = 0.0;
+        a44[3][0] = 0.0;       a44[3][1] = 0.0;      a44[3][2] = 0.0; a44[3][3] = 1.0;
 
-	Matrix4x4 A = new Matrix4x4(a44);
+        Matrix4x4 A = new Matrix4x4(a44);
 
-	TransformationMatrix = TransformationMatrix.multiply4x4(A);
+        TransformationMatrix = TransformationMatrix.multiply4x4(A);
     }
 
     public void clearScene() throws Exception{
-	screen.drawRaster(zBuffer.clear());
+        screen.drawRaster(zBuffer.clear());
     }
 
     public void clearRaster() throws Exception{
-	zBuffer.clear();
+        zBuffer.clear();
     }
     
     public <T> void updateScene(ArrayList<T> geoms) throws Exception{
-	T geom;
-	
-	Iterator<T> geomsIterator = geoms.iterator();
-	
-	while (geomsIterator.hasNext()){
-	    geom = geomsIterator.next();
-	    updateScene(geom);
-	}
+        T geom;
+
+        Iterator<T> geomsIterator = geoms.iterator();
+
+        while (geomsIterator.hasNext()){
+            geom = geomsIterator.next();
+            updateScene(geom);
+        }
     }
     
     public <T> void updateScene(T geom) throws Exception{
-	ArrayList<T> geoms = new ArrayList<T>(){{add(geom);}};
-	ArrayList<T> transformedGeoms, clippedGeoms;
-	ArrayList<T> granularGeoms;
-	ArrayList<T> projectedGeoms;
-	double epsilon = 150.0;
+        ArrayList<T> geoms = new ArrayList<T>(){{add(geom);}};
+        ArrayList<T> transformedGeoms, clippedGeoms;
+        ArrayList<T> granularGeoms;
+        ArrayList<T> projectedGeoms;
+        double epsilon = 150.0;
 
-	transformedGeoms = Util3d.transformGeoms(geoms, TransformationMatrix);
-	clippedGeoms = Util3d.clipGeoms(transformedGeoms, cameraBoundingVolume);
-	granularGeoms = Util3d.ensureGranularity(clippedGeoms, epsilon);
-	projectedGeoms = Util3d.projectGeoms(granularGeoms, ProjectionPlaneValue);
-	zBuffer.rasterizeGeoms(projectedGeoms, ProjectionWindowWidth, ProjectionWindowHeight);
+        transformedGeoms = Util3d.transformGeoms(geoms, TransformationMatrix);
+        clippedGeoms = Util3d.clipGeoms(transformedGeoms, cameraBoundingVolume);
+        granularGeoms = Util3d.ensureGranularity(clippedGeoms, epsilon);
+        projectedGeoms = Util3d.projectGeoms(granularGeoms, ProjectionPlaneValue);
+        zBuffer.rasterizeGeoms(projectedGeoms, ProjectionWindowWidth, ProjectionWindowHeight);
     }
 
     public void showScene() throws Exception{
-	screen.drawRaster(zBuffer.getRaster());
+        screen.drawRaster(zBuffer.getRaster());
     }
 }
--- Screen.java
+++ Screen.java
@@ -6,105 +6,109 @@ import java.awt.image.*;
 
 public class Screen extends Frame{
 
-    Image image = null;
-    int W = 0, H = 0;
+  Image image = null;
+  int W = 0, H = 0;
 
-    Color background;
+  Color background;
 
-    Screen(int W, int H){
-	this.W = W; this.H = H;
-	setSize(W, H);
-	setUndecorated(false);
-	setTitle("Aiamg");
-	setVisible(true);
+  Screen(int W, int H){
+      this(W, H, W/2, H/2);
+  }
 
-	this.addWindowListener(
-			       new WindowAdapter(){
-				   public void windowClosing(WindowEvent e){
-				       System.exit(0);
-				   }
-			       }
-			       );
+  Screen(int W, int H, int locationX, int locationY){
+    this.W = W; this.H = H;
+	  setSize(W, H);
+	  setUndecorated(false);
+	  setTitle("Aiamg");
+    setLocation(locationX, locationY);
+	  setVisible(true);
 
+	  this.addWindowListener(
+		  new WindowAdapter(){
+			                     public void windowClosing(WindowEvent e){
+				                                                             System.exit(0);
+				                                                           }
+			                   }
+		);
 
-	image = createImage(W, H);
-	initScreen();
-	Insets in = getInsets();
-	setSize(W, H);
-	getGraphics().drawImage(image, in.left, in.top, null);
-	Cursor crosshairCursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
-	int[] myPixels = new int[16 * 16];
-	Image thecursorimage = createImage(new MemoryImageSource(16, 16, myPixels, 0, 16));
-	Cursor hiddenCursor = (getToolkit()).createCustomCursor(thecursorimage,
-		 					        new Point(0, 0),
-								"invisiblecursor");
-	setCursor(crosshairCursor);
-	background = Color.lightGray;
+	  image = createImage(W, H);
+	  initScreen();
+	  Insets in = getInsets();
+	  setSize(W, H);
+	  getGraphics().drawImage(image, in.left, in.top, null);
+	  Cursor crosshairCursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
+	  int[] myPixels = new int[16 * 16];
+	  Image thecursorimage = createImage(new MemoryImageSource(16, 16, myPixels, 0, 16));
+	  Cursor hiddenCursor = (getToolkit()).createCustomCursor(thecursorimage,
+                           new Point(0, 0),
+								           "invisiblecursor");
+	  setCursor(crosshairCursor);
+	  background = Color.lightGray;
 
-	repaint();
-    }
+	  repaint();
+  }
 
-    protected void initScreen(){
-	Graphics sg = image.getGraphics();
-	sg.setClip(0, 0, W, H);
-	sg.setColor(background);
-	sg.fillRect(0, 0, W, H);
-	sg.dispose();
-    }
+  protected void initScreen(){
+	  Graphics sg = image.getGraphics();
+	  sg.setClip(0, 0, W, H);
+	  sg.setColor(background);
+	  sg.fillRect(0, 0, W, H);
+	  sg.dispose();
+  }
 
-    public int getW(){
-	return W;
-    }
+  public int getW(){
+	  return W;
+  }
 
-    public int getH(){
-	return H;
-    }
+  public int getH(){
+	  return H;
+  }
 
-    public Image createImage(){
-	return createImage(W, H);
-    }
+  public Image createImage(){
+	  return createImage(W, H);
+  }
 
-    public void update(Graphics g){
-	paint(g);
-    }
+  public void update(Graphics g){
+	  paint(g);
+  }
 
-    public void paint(Graphics g){
-	try{
-	    Insets in = this.getInsets();
-	    g.translate(in.left, in.top);
-	    g.drawImage(image, 0,0, null);
-	} catch(Exception e){}
-    }
+  public void paint(Graphics g){
+	  try{
+	      Insets in = this.getInsets();
+	      g.translate(in.left, in.top);
+	      g.drawImage(image, 0,0, null);
+	  } catch(Exception e){}
+  }
 
-    public void drawRaster(RasterPoint[][] raster) throws Exception{
-	image = createImage(W, H);  // image is off screen for double buffering
-	Graphics ig = image.getGraphics();
+  public void drawRaster(RasterPoint[][] raster) throws Exception{
+	  image = createImage(W, H);  // image is off screen for double buffering
+	  Graphics ig = image.getGraphics();
 
-	for (int i = 0; i < W; i++){
-	    for (int j = 0; j < H; j++){
-		ig.setColor((raster[i][j]).fadedColor);
-		ig.drawLine(i, H - j - 1, i, H - j - 1); //Reversed on y-axis
-	    }
-	}
+	  for (int i = 0; i < W; i++){
+	      for (int j = 0; j < H; j++){
+		      ig.setColor((raster[i][j]).fadedColor);
+		      ig.drawLine(i, H - j - 1, i, H - j - 1); //Reversed on y-axis
+	      }
+	  }
 	
-	update(getGraphics());
-	Thread.sleep(1); //Delay added because of SocketExceptions
-    }
+	  update(getGraphics());
+	  Thread.sleep(1); //Delay added because of SocketExceptions
+  }
 
-    public void drawDecorations(){
-	image = createImage(W, H);  // image is off screen for double buffering
-	Graphics ig = image.getGraphics();
+  public void drawDecorations(){
+	  image = createImage(W, H);  // image is off screen for double buffering
+	  Graphics ig = image.getGraphics();
 
-	// Crosshair:
-	ig.setColor(Color.cyan);
-	ig.drawLine(W / 2 - 10, H / 2, W / 2 + 10, H / 2);
-	ig.drawLine(W / 2, H / 2 - 10, W / 2, H / 2 + 10);
+	  // Crosshair:
+	  ig.setColor(Color.cyan);
+	  ig.drawLine(W / 2 - 10, H / 2, W / 2 + 10, H / 2);
+	  ig.drawLine(W / 2, H / 2 - 10, W / 2, H / 2 + 10);
 
-	update(getGraphics());
-    }
+	  update(getGraphics());
+  }
 
-    public void paintImage(Image _image){
-	image = _image;
-	update(getGraphics());
-    }
+  public void paintImage(Image _image){
+	  image = _image;
+	  update(getGraphics());
+  }
 }
